23921ddd56f9a0f50012391cad96aaf5e99a4a25
EduardNovik <eddnovik@gmail.com>
2025-03-16 20:18:31 +0100

    initial commit

---
+++ packages/container/src/components/MarketingApp.js
@@ -0,0 +1,14 @@
+import { useRef, useEffect } from "react";
+
+import React from "react";
+import { mount } from "marketing/MarketingApp";
+
+export default () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    mount(ref.current);
+  });
+
+  return <div ref={ref}></div>;
+};
